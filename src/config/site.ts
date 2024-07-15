c3b7eb7f3523fa9ee20dd25772eca7769b68f6e2
No0ne003 <hi.yassir.op@gmail.com>
2024-07-15 12:17:19 +0100

    adding config file

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Urba events international",
+  url: "http://urbaevents.com/",
+  description: "urba events international",
+  links: {
+    facebook: "https://www.facebook.com/Urbaevents",
+    youtube: "https://www.youtube.com/channel/UCjOn8gAB3zTDIwty43s2SCg",
+    instagram: "https://www.instagram.com/urba_events/",
+    email: "contact@urbaevents.com",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
