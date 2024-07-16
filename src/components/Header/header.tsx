675d8834bf5d0a5cd5a253e5b44400924908f610
No0ne003 <hi.yassir.op@gmail.com>
2024-07-16 13:35:56 +0100

    mainNav container

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { Icons } from "../icons";
+import { MainNav } from "./MainNav";
 
 export default function Header() {
   return (
     <header className="z-10 w-screen border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <UrbaLogo />
+        <div className="flex-1 flex items-center justify-end space-x-2">
+          <MainNav />
+        </div>
       </div>
     </header>
   );
