a77a620b75d6d32094bbeb432ad332818419ad29
No0ne003 <hi.yassir.op@gmail.com>
2024-07-17 14:45:03 +0100

    feat: intro animation (i will do v2 soon)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex-1 flex justify-center items-center">
-      <h1 className="text-9xl font-bold font-mono">Welcome.</h1>
+    <div className="container flex-1 flex justify-center items-center">
+      <h1 className="text-7xl font-bold font-mono">Welcome.</h1>
     </div>
   );
 }
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -9,66 +9,38 @@ export default function Intro() {
   useLayoutEffect(() => {
     let mm = gsap.matchMedia();
 
-    // Define media query for large screens
-    mm.add("(min-width: 1024px)", () => {
-      let ctx = gsap.context(() => {
-        const t1 = gsap.timeline();
-        t1.to(["#title-span-1", "#title-span-2", "#title-span-3"], {
-          opacity: 1,
-          y: "+=30",
-          delay: 0.5,
-          stagger: 0.8,
+    let ctx = gsap.context(() => {
+      const t1 = gsap.timeline();
+      t1.to("#intro-logo", {
+        scale: 1,
+      })
+        .to("#intro-logo", {
+          scale: 1.1,
+          delay: 0.1,
+          duration: 0.2,
         })
-          .to("#intro-slider", {
-            background: "#B31E8E",
-          })
-          .to("#title-span-2", {
-            x: "-=100",
-          })
-          .to("#title-span-3", {
-            x: "+=500",
-          })
-          .to("#intro-logo", {
-            yPercent: "100",
-            delay: 0.5,
-            duration: 1.5,
-          })
-          .to("#intro-slider", {
-            yPercent: "-100",
-            delay: 2,
-            duration: 1.5,
-          });
-      }, comp);
+        .to("#intro-logo", {
+          scale: 1,
+        })
+        .to("#intro-slider", {
+          yPercent: "-100",
+          delay: 0.5,
+          duration: 1,
+        });
+    }, comp);
 
-      return () => ctx.revert();
-    });
-
-    return () => mm.revert();
+    return () => ctx.revert();
   }, []);
 
   return (
     <div className="relative" ref={comp}>
       <div
         id="intro-slider"
-        className="absolute h-screen w-full p-10 bg-gray-200 text-background top-0 left-0 font-mono z-10 flex flex-col items-center justify-center gap-10 overflow-hidden"
+        className="absolute h-screen w-full p-10 bg-primary text-background top-0 left-0 font-mono z-10 flex flex-col items-center justify-center gap-10 overflow-hidden"
       >
-        <h1
-          id="main-title"
-          className="text-5xl md:text-8xl flex flex-col gap-5 font-bold"
-        >
-          <span id="title-span-1" className="opacity-0">
-            Urba
-          </span>
-          <span id="title-span-2" className="opacity-0">
-            Events
-          </span>
-          <span id="title-span-3" className="opacity-0">
-            International
-          </span>
-        </h1>
         <Icons.tei
           id="intro-logo"
-          className="fill-background h-screen absolute top-[-100%] scale-[1.5]"
+          className="fill-backgroundself-center size-72 scale-0"
         />
       </div>
     </div>
