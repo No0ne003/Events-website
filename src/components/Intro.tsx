f956392e59151e106f8a02d47fdc6c649f2b3958
No0ne003 <hi.yassir.op@gmail.com>
2024-07-17 12:12:54 +0100

    finish testing animation

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -8,6 +8,22 @@ export default function Intro() {
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
       const t1 = gsap.timeline();
+      t1.from(["#title-span-1", "#title-span-2", "#title-span-3"], {
+        opacity: 0,
+        y: "+=30",
+        delay: 0.5,
+        stagger: 0.8,
+      })
+        .to(["#title-span-1", "#title-span-2", "#title-span-3"], {
+          opacity: 0,
+          y: "-=30",
+          delay: 0.8,
+          stagger: 0.5,
+        })
+        .to("#intro-slider", {
+          yPercent: "-100",
+          duration: 1.5,
+        });
     }, comp);
 
     return () => ctx.revert();
@@ -17,16 +33,12 @@ export default function Intro() {
     <div className="relative" ref={comp}>
       <div
         id="intro-slider"
-        className="h-screen p-10 bg-gray-50 absolute top-0 left-0 font-mono z-10 w-full flex flex-col gap-10 text-black"
+        className="h-screen p-10 bg-gray-50 absolute top-0 left-0 font-mono z-10 w-full flex flex-col items-center justify-center gap-10 text-black"
       >
-        <h1 id="title-1" className="text-9xl">
-          Test The Test
-        </h1>
-        <h1 id="title-2" className="text-9xl">
-          Apps From Appel
-        </h1>
-        <h1 id="title-3" className="text-9xl">
-          Bug Test
+        <h1 id="main-title" className="text-8xl flex flex-col gap-5">
+          <span id="title-span-1">Urba</span>
+          <span id="title-span-2">Events</span>
+          <span id="title-span-3">International</span>
         </h1>
       </div>
     </div>
