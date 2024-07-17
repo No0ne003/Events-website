0564f0586f7a5d0052a0ea599ffbc5d647b49328
No0ne003 <hi.yassir.op@gmail.com>
2024-07-17 11:39:12 +0100

    setup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="flex-1 flex justify-center items-center">
+      <h1 className="text-9xl font-bold font-mono">Welcome.</h1>
+    </div>
+  );
 }
