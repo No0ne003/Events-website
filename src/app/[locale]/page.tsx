ec337034036abc9ab46601afe6dc208ba61652e8
No0ne003 <hi.yassir.op@gmail.com>
2024-07-27 15:48:38 +0100

    feat: simple hero button

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,6 +5,8 @@ import ImageAnimation from "@/components/Home/ImageAnimation";
 import TitleAnimation from "@/components/Home/TitleAnimation";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const Home: React.FC = () => {
   const locale = useLocale();
@@ -21,13 +23,33 @@ const Home: React.FC = () => {
   ];
 
   const title = t("title");
+  const localActive = useLocale();
 
   return (
     <div className="flex-1">
       <div className="container flex flex-col justify-center items-center relative size-full">
         <div className="flex flex-col gap-10 justify-center items-center w-screen h-[400px]">
           <TitleAnimation title={title} locale={locale} />
-          {/* <Button>Contact Us</Button> */}
+          <motion.div
+            initial={{ y: "-100%", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              delay: 6,
+              duration: 0.8,
+              ease: [0.19, 1, 0.22, 1],
+            }}
+            className={cn(
+              "flex gap-8 jusitfy-center items-center",
+              localActive === "ar" ? "flex-row-reverse" : null,
+            )}
+          >
+            <Button variant={"secondary"} size="xl" asChild>
+              <Link href={`${localActive}/contact`}>{t("contact-us")}</Link>
+            </Button>
+            <Button variant={"outline"} size="lg">
+              <Link href={`${localActive}/services`}>{t("discover-now")}</Link>
+            </Button>
+          </motion.div>
         </div>
         <div className="flex justify-center items-center w-full">
           <ImageAnimation images={images} />
