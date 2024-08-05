53090fd72a795f480721952db489b4bbe196700d
No0ne003 <hi.yassir.op@gmail.com>
2024-08-05 12:56:10 +0100

    style: change gap

--- src/app/[locale]/_components/Home/Refrences/RefrencesItem.tsx
+++ src/app/[locale]/_components/Home/Refrences/RefrencesItem.tsx
@@ -7,7 +7,7 @@ interface RefrencesItemProps {
 
 const RefrencesItem: React.FC<RefrencesItemProps> = ({ image }) => {
   return (
-    <div className="group relative overflow-hidden h-[100px] w-[40px] md:w-[60px] mx-5 md:mx-12 flex justify-center items-center">
+    <div className="group relative overflow-hidden h-[100px] w-[40px] md:w-[60px] mx-5 md:mx-8 lg:mx-12 flex justify-center items-center">
       <Image src={image} alt={image} width={130} height={600} />
     </div>
   );
