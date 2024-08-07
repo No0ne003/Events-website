33ac041785c1443c191f7738380460c4646f64b7
No0ne003 <hi.yassir.op@gmail.com>
2024-08-07 12:14:23 +0100

    style: reverse arrow position on arabic local

--- src/app/[locale]/_components/Home/Hero-section/HeroButtons.tsx
+++ src/app/[locale]/_components/Home/Hero-section/HeroButtons.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { cn, fadeInUp, transitionSettings } from "@/lib/utils";
 import Link from "next/link";
-import { ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default function HeroButton({
@@ -30,10 +30,24 @@ export default function HeroButton({
         className="bg-primary hover:bg-primary/90 group relative"
         asChild
       >
-        <Link href={`${locale}/contact`}>
+        <Link
+          href={`${locale}/contact`}
+          className={cn("flex", locale === "ar" ? "flex-row-reverse" : null)}
+        >
           {contactUs}
-          <div className="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-4 group-hover:translate-x-0 group-hover:pl-3 group-hover:opacity-100">
-            <ArrowRight className="size-4" />
+          <div
+            className={cn(
+              "w-0 translate-x-[100%] opacity-0 transition-all duration-200 group-hover:w-4 group-hover:translate-x-0 group-hover:opacity-100",
+              locale === "ar"
+                ? "group-hover:pr-3 pr-0 group-hover:translate-x-[-50%]"
+                : "group-hover:pl-3 pl-0",
+            )}
+          >
+            {locale === "ar" ? (
+              <ArrowLeft className="size-4" />
+            ) : (
+              <ArrowRight className="size-4" />
+            )}
           </div>
         </Link>
       </Button>
