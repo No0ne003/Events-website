34be1dc7fc976df13e980ce4c1d40a31e4df2017
No0ne003 <hi.yassir.op@gmail.com>
2024-08-12 21:43:50 +0100

    fix: Icons.logo props

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -37,7 +37,7 @@ export function MobileNav() {
           href="/"
           className="flex items-center"
         >
-          <Icons.logo className="mr-2" />{" "}
+          <Icons.logo src="/logo-urba.png" alt="urba logo" className="mr-2" />
           <span className="sr-only">{siteConfig.name}</span>
         </MobileLink>
         <div className="flex flex-col gap-3 mt-5">
