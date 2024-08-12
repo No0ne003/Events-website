b91daea0bcdc25900d5b2ea8c205eafcd76e7a20
No0ne003 <hi.yassir.op@gmail.com>
2024-08-12 14:42:24 +0100

    chore: setup quote section

---
+++ src/app/[locale]/_components/Home/Quote/Quote.tsx
@@ -0,0 +1,7 @@
+export default function Quote() {
+  return (
+    <section className="py-5 bg-gradient-to-b from-primary/20 to-transparent">
+      <div className="container">quote</div>
+    </section>
+  );
+}
