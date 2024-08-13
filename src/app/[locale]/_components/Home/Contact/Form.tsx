47749337cb3c90f6383af0872f5bf83c3e3a5c14
No0ne003 <hi.yassir.op@gmail.com>
2024-08-13 15:14:35 +0100

    chore: adding validation

--- src/app/[locale]/_components/Home/Contact/Form.tsx
+++ src/app/[locale]/_components/Home/Contact/Form.tsx
@@ -1,67 +1,120 @@
-import { Input } from "@/components/ui/input";
+"use client";
+import { useState } from "react";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
+import { Textarea, TextareaClassName } from "@/components/ui/textarea";
+import { validationSchema } from "@/lib/validation";
+import { toFormikValidationSchema } from "zod-formik-adapter";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { InputClassName } from "@/components/ui/input";
+
+type FormValues = {
+  name: string;
+  email: string;
+  mobileNumber: number;
+  headOffice: string;
+  message: string;
+};
 
 export default function ContactForm() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (values: FormValues) => {
+    console.log(values);
+  };
+
   return (
-    <form className="container max-w-3xl py-8 space-y-6">
-      <div className="flex flex-wrap -mx-5">
-        <div className="w-full md:w-1/2 px-5 mb-4 space-y-2">
-          <Label className="bg-primary/5" htmlFor="name">
-            Name
-          </Label>
-          <Input
-            className="bg-primary/5"
-            type="text"
-            id="name"
-            name="name"
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        mobileNumber: 0,
+        headOffice: "",
+        message: "",
+      }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className="container max-w-3xl py-8 space-y-6">
+        <div className="flex flex-wrap -mx-5">
+          <div className="w-full md:w-1/2 px-5 mb-4 space-y-2">
+            <Label htmlFor="name">Name</Label>
+            <Field
+              className={InputClassName}
+              type="text"
+              id="name"
+              name="name"
+              required
+            />
+            <ErrorMessage
+              name="name"
+              component="div"
+              className="text-rose-700"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-5 mb-4 space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Field
+              className={InputClassName}
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="text-rose-700"
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-5">
+          <div className="w-full md:w-1/2 px-5 mb-4 space-y-2">
+            <Label htmlFor="mobileNumber">Mobile Number</Label>
+            <Field
+              className={InputClassName}
+              type="number"
+              id="mobileNumber"
+              name="mobileNumber"
+              required
+            />
+            <ErrorMessage
+              name="mobileNumber"
+              component="div"
+              className="text-rose-700"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-5 mb-4 space-y-2">
+            <Label htmlFor="headOffice">Head Office</Label>
+            <Field
+              className={InputClassName}
+              type="text"
+              id="headOffice"
+              name="headOffice"
+              required
+            />
+            <ErrorMessage
+              name="headOffice"
+              component="div"
+              className="text-rose-700"
+            />
+          </div>
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="message">Message</Label>
+          <Field
+            className={TextareaClassName}
+            id="message"
+            name="message"
+            as="textarea"
             required
           />
-        </div>
-        <div className="w-full md:w-1/2 px-5 mb-4 space-y-2">
-          <Label className="bg-primary/5" htmlFor="email">
-            Email
-          </Label>
-          <Input
-            className="bg-primary/5"
-            type="email"
-            id="email"
-            name="email"
-            required
+          <ErrorMessage
+            name="message"
+            component="div"
+            className="text-rose-700"
           />
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-5">
-        <div className="w-full md:w-1/2 px-5 mb-4 space-y-2">
-          <Label htmlFor="mobileNumber">Mobile Number</Label>
-          <Input
-            className="bg-primary/5"
-            type="number"
-            id="mobileNumber"
-            name="mobileNumber"
-            required
-          />
-        </div>
-        <div className="w-full md:w-1/2 px-5 mb-4 space-y-2">
-          <Label htmlFor="headOffice">Head Office</Label>
-          <Input
-            className="bg-primary/5"
-            type="text"
-            id="headOffice"
-            name="headOffice"
-            required
-          />
-        </div>
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="message">Message</Label>
-        <Textarea
-          className="bg-primary/5"
-          id="message"
-          name="message"
-          required
-        />
-      </div>
-    </form>
+      </Form>
+    </Formik>
   );
 }
