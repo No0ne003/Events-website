c8eae62fc91e898c6b254bae9c8f7320165bc6ab
No0ne003 <hi.yassir.op@gmail.com>
2024-08-23 21:47:23 +0100

    chore: setup footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-primary text-primary-foreground py-16">
+      <div className="container">test</div>
+    </footer>
+  );
+}
