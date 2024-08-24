7055da72ad236b3f0b001752b3bc725a8ef8847f
No0ne003 <hi.yassir.op@gmail.com>
2024-08-24 16:37:20 +0100

    style: better responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,38 +40,36 @@ export default function Footer() {
 
   return (
     <footer className="bg-primary text-primary-foreground py-12">
-      <div className="container">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-14">
-          <div className="flex flex-col items-center md:items-start gap-5">
-            <div>
-              <Icons.logo
-                src="/logo-urba2.png"
-                alt="Urba logo"
-                className="mb-4"
-              />
-              <div className="text-center text-xs text-primary-foreground/60">
-                Copyright &copy; {new Date().getFullYear()} - All rights
-                reserved.
-              </div>
-            </div>
-            <div className="flex gap-4">
-              {socialLinks.map(({ href, label, icon }) => (
-                <Link
-                  key={label}
-                  target="_blank"
-                  rel="noreferrer"
-                  href={href}
-                  aria-label={label}
-                  className="transition-transform hover:scale-110 p-2 border-[1px] border-primary-foreground/40 rounded-full"
-                >
-                  {icon}
-                </Link>
-              ))}
+      <div className="container flex flex-col gap-14 md:flex-row justify-evenly ">
+        <div className="flex flex-col items-start gap-5 md:px-14">
+          <div className="flex flex-col md:items-center">
+            <Icons.logo
+              src="/logo-urba2.png"
+              alt="Urba logo"
+              className="mb-4"
+            />
+            <div className="text-center text-xs text-primary-foreground/60">
+              Copyright &copy; {new Date().getFullYear()} - All rights reserved.
             </div>
           </div>
-
-          <div className="text-center md:text-left">
-            <h4 className="text-base font-semibold mb-4">{t("explore")}</h4>
+          <div className="flex gap-4">
+            {socialLinks.map(({ href, label, icon }) => (
+              <Link
+                key={label}
+                target="_blank"
+                rel="noreferrer"
+                href={href}
+                aria-label={label}
+                className="transition-transform hover:scale-110 p-2 border-[1px] border-primary-foreground/40 rounded-full"
+              >
+                {icon}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="w-full flex justify-start gap-8 md:px-14">
+          <div className="text-left flex flex-col gap-3 w-full">
+            <h4 className="text-base font-semibold">{t("explore")}</h4>
             <div className="flex flex-col gap-2">
               {links.map((link) => (
                 <Link
@@ -85,9 +83,9 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className="text-center md:text-left">
-            <h4 className="text-base font-semibold mb-4">{t("contact")}</h4>
-            <address className="not-italic text-sm leading-6">
+          <div className="text-left flex flex-col gap-3 w-full">
+            <h4 className="text-base font-semibold">{t("contact")}</h4>
+            <address className="not-italic text-sm leading-6 flex flex-col gap-1">
               <a
                 href={`mailto:${siteConfig.email}`}
                 className="transition-colors hover:text-primary-foreground/80"
@@ -101,10 +99,6 @@ export default function Footer() {
               >
                 {siteConfig.tel}
               </a>
-              <br />
-              {siteConfig.address.map((line, index) => (
-                <p key={index}>{line}</p>
-              ))}
             </address>
           </div>
         </div>
