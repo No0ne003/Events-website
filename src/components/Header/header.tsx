c151ff8da12a3a8c4cee50d40fd0d93704472256
No0ne003 <hi.yassir.op@gmail.com>
2024-08-25 16:42:20 +0100

    fix(header): local-switcher & logo doesn't click (cuz of the overlay z-index)

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -18,6 +18,7 @@ export default function Header() {
 
   return (
     <header className="relative py-4 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      {/* Adjusted z-index to ensure it's behind other elements */}
       <motion.div
         initial={{ y: "-300%" }}
         animate={{ y: 0 }}
@@ -25,7 +26,7 @@ export default function Header() {
           duration: 0.8,
           ease: [0.19, 1, 0.22, 1],
         }}
-        className="absolute top-0 h-[90px] w-screen bg-gradient-to-b from-primary/20 to-transparent"
+        className="absolute top-0 h-[90px] w-screen bg-gradient-to-b from-primary/20 to-transparent z-10"
       />
       <motion.div
         initial="hidden"
@@ -38,7 +39,7 @@ export default function Header() {
         className="container flex h-14 max-w-screen-2xl items-center"
       >
         <UrbaLogo />
-        <div className="flex-1 flex items-center justify-end space-x-8 font-spectral">
+        <div className="flex-1 flex items-center justify-end space-x-8 font-spectral z-30">
           <MainNav />
           <LocalSwitcher />
           <MobileNav />
@@ -50,7 +51,7 @@ export default function Header() {
 
 export function UrbaLogo() {
   return (
-    <Link href="/" className="flex self-end shadow-primary">
+    <Link href="/" className="flex self-end shadow-primary z-30">
       <Icons.logo src="/logo-urba.png" alt="urba logo" className="mr-2" />
       <span className="sr-only">{siteConfig.name}</span>
     </Link>
